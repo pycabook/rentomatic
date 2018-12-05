e24c397242565c493286881dd186df05538cbd9f
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-05 13:29:41 +0000

    Chapter 2 - Domain models - Step 2

--- rentomatic/domain/room.py
+++ rentomatic/domain/room.py
@@ -5,3 +5,13 @@ class Room:
         self.price = price
         self.latitude = latitude
         self.longitude = longitude
+
+    @classmethod
+    def from_dict(cls, adict):
+        return cls(
+            code=adict['code'],
+            size=adict['size'],
+            price=adict['price'],
+            latitude=adict['latitude'],
+            longitude=adict['longitude'],
+        )
--- tests/domain/test_room.py
+++ tests/domain/test_room.py
@@ -12,3 +12,21 @@ def test_room_model_init():
     assert room.price == 10
     assert room.longitude == -0.09998975
     assert room.latitude == 51.75436293
+
+
+def test_room_model_from_dict():
+    code = uuid.uuid4()
+    room = r.Room.from_dict(
+        {
+            'code': code,
+            'size': 200,
+            'price': 10,
+            'longitude': -0.09998975,
+            'latitude': 51.75436293
+        }
+    )
+    assert room.code == code
+    assert room.size == 200
+    assert room.price == 10
+    assert room.longitude == -0.09998975
+    assert room.latitude == 51.75436293
