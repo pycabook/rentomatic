aa44227fc2d33eaa10321a637095d4b396abb80d
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-05 13:27:31 +0000

    Chapter 2 - Domain models - Step 1

---
+++ rentomatic/domain/room.py
@@ -0,0 +1,7 @@
+class Room:
+    def __init__(self, code, size, price, longitude, latitude):
+        self.code = code
+        self.size = size
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
---
+++ tests/domain/test_room.py
@@ -0,0 +1,14 @@
+import uuid
+from rentomatic.domain import room as r
+
+
+def test_room_model_init():
+    code = uuid.uuid4()
+    room = r.Room(code, size=200, price=10,
+                  longitude=-0.09998975,
+                  latitude=51.75436293)
+    assert room.code == code
+    assert room.size == 200
+    assert room.price == 10
+    assert room.longitude == -0.09998975
+    assert room.latitude == 51.75436293
