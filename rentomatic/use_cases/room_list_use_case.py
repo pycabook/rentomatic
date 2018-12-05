6a621fd203804ec40a2f01d6296e4f6c14744356
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-05 19:58:15 +0000

    Chapter 2 - Use cases

---
+++ rentomatic/use_cases/room_list_use_case.py
@@ -0,0 +1,7 @@
+class RoomListUseCase:
+
+    def __init__(self, repo):
+        self.repo = repo
+
+    def execute(self):
+        return self.repo.list()
---
+++ tests/use_cases/test_room_list_use_case.py
@@ -0,0 +1,54 @@
+import pytest
+import uuid
+from unittest import mock
+
+from rentomatic.domain import room as r
+from rentomatic.use_cases import room_list_use_case as uc
+
+
+@pytest.fixture
+def domain_rooms():
+    room_1 = r.Room(
+        code=uuid.uuid4(),
+        size=215,
+        price=39,
+        longitude=-0.09998975,
+        latitude=51.75436293,
+    )
+
+    room_2 = r.Room(
+        code=uuid.uuid4(),
+        size=405,
+        price=66,
+        longitude=0.18228006,
+        latitude=51.74640997,
+    )
+
+    room_3 = r.Room(
+        code=uuid.uuid4(),
+        size=56,
+        price=60,
+        longitude=0.27891577,
+        latitude=51.45994069,
+    )
+
+    room_4 = r.Room(
+        code=uuid.uuid4(),
+        size=93,
+        price=48,
+        longitude=0.33894476,
+        latitude=51.39916678,
+    )
+
+    return [room_1, room_2, room_3, room_4]
+
+
+def test_room_list_without_parameters(domain_rooms):
+    repo = mock.Mock()
+    repo.list.return_value = domain_rooms
+
+    room_list_use_case = uc.RoomListUseCase(repo)
+    result = room_list_use_case.execute()
+
+    repo.list.assert_called_with()
+    assert result == domain_rooms
