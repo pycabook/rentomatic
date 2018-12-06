a8eadaea39f91feeb8b6d0ae49b6b6d33ed7689a
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-06 18:25:17 +0000

    Chapter 3 - Request validation

--- rentomatic/request_objects/room_list_request_object.py
+++ rentomatic/request_objects/room_list_request_object.py
@@ -1,7 +1,55 @@
-class RoomListRequestObject:
+import collections
+
+
+class InvalidRequestObject:
+
+    def __init__(self):
+        self.errors = []
+
+    def add_error(self, parameter, message):
+        self.errors.append({'parameter': parameter, 'message': message})
+
+    def has_errors(self):
+        return len(self.errors) > 0
+
+    def __bool__(self):
+        return False
+
+
+class ValidRequestObject:
+
     @classmethod
     def from_dict(cls, adict):
-        return cls()
+        raise NotImplementedError
 
     def __bool__(self):
         return True
+
+
+class RoomListRequestObject(ValidRequestObject):
+
+    accepted_filters = ['code__eq', 'price__eq', 'price__lt', 'price__gt']
+
+    def __init__(self, filters=None):
+        self.filters = filters
+
+    @classmethod
+    def from_dict(cls, adict):
+        invalid_req = InvalidRequestObject()
+
+        if 'filters' in adict:
+            if not isinstance(adict['filters'], collections.Mapping):
+                invalid_req.add_error('filters', 'Is not iterable')
+                return invalid_req
+
+            for key, value in adict['filters'].items():
+                if key not in cls.accepted_filters:
+                    invalid_req.add_error(
+                        'filters',
+                        'Key {} cannot be used'.format(key)
+                    )
+
+        if invalid_req.has_errors():
+            return invalid_req
+
+        return cls(filters=adict.get('filters', None))
--- tests/request_objects/test_room_list_request_objects.py
+++ tests/request_objects/test_room_list_request_objects.py
@@ -1,13 +1,74 @@
+import pytest
+
 from rentomatic.request_objects import room_list_request_object as req
 
 
 def test_build_room_list_request_object_without_parameters():
     request = req.RoomListRequestObject()
 
+    assert request.filters is None
     assert bool(request) is True
 
 
 def test_build_room_list_request_object_from_empty_dict():
     request = req.RoomListRequestObject.from_dict({})
 
+    assert request.filters is None
     assert bool(request) is True
+
+
+def test_build_room_list_request_object_with_empty_filters():
+    request = req.RoomListRequestObject(filters={})
+
+    assert request.filters == {}
+    assert bool(request) is True
+
+
+def test_build_room_list_request_object_from_dict_with_empty_filters():
+    request = req.RoomListRequestObject.from_dict({'filters': {}})
+
+    assert request.filters == {}
+    assert bool(request) is True
+
+
+def test_build_room_list_request_object_from_dict_with_filters_wrong():
+    request = req.RoomListRequestObject.from_dict({'filters': {'a': 1}})
+
+    assert request.has_errors()
+    assert request.errors[0]['parameter'] == 'filters'
+    assert bool(request) is False
+
+
+def test_build_room_list_request_object_from_dict_with_invalid_filters():
+    request = req.RoomListRequestObject.from_dict({'filters': 5})
+
+    assert request.has_errors()
+    assert request.errors[0]['parameter'] == 'filters'
+    assert bool(request) is False
+
+
+@pytest.mark.parametrize(
+    'key',
+    ['code__eq', 'price__eq', 'price__lt', 'price__gt']
+)
+def test_build_room_list_request_object_accepted_filters(key):
+    filters = {key: 1}
+
+    request = req.RoomListRequestObject.from_dict({'filters': filters})
+
+    assert request.filters == filters
+    assert bool(request) is True
+
+
+@pytest.mark.parametrize(
+    'key',
+    ['code__lt', 'code__gt']
+)
+def test_build_room_list_request_object_rejected_filters(key):
+    filters = {key: 1}
+
+    request = req.RoomListRequestObject.from_dict({'filters': filters})
+
+    assert request.has_errors()
+    assert request.errors[0]['parameter'] == 'filters'
+    assert bool(request) is False
