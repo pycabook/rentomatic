8e4911f625c7cdca4ad4b8dc32693f3cc849cb61
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-07 08:42:11 +0000

    Chapter 3 - Responses and failures

--- rentomatic/response_objects/response_objects.py
+++ rentomatic/response_objects/response_objects.py
@@ -1,6 +1,48 @@
+class ResponseFailure:
+    RESOURCE_ERROR = 'ResourceError'
+    PARAMETERS_ERROR = 'ParametersError'
+    SYSTEM_ERROR = 'SystemError'
+
+    def __init__(self, type_, message):
+        self.type = type_
+        self.message = self._format_message(message)
+
+    def _format_message(self, msg):
+        if isinstance(msg, Exception):
+            return "{}: {}".format(msg.__class__.__name__, "{}".format(msg))
+        return msg
+
+    @property
+    def value(self):
+        return {'type': self.type, 'message': self.message}
+
+    def __bool__(self):
+        return False
+
+    @classmethod
+    def build_from_invalid_request_object(cls, invalid_request_object):
+        message = "\n".join(["{}: {}".format(err['parameter'], err['message'])
+                             for err in invalid_request_object.errors])
+        return cls(cls.PARAMETERS_ERROR, message)
+
+    @classmethod
+    def build_resource_error(cls, message=None):
+        return cls(cls.RESOURCE_ERROR, message)
+
+    @classmethod
+    def build_system_error(cls, message=None):
+        return cls(cls.SYSTEM_ERROR, message)
+
+    @classmethod
+    def build_parameters_error(cls, message=None):
+        return cls(cls.PARAMETERS_ERROR, message)
+
+
 class ResponseSuccess:
+    SUCCESS = 'Success'
 
     def __init__(self, value=None):
+        self.type = self.SUCCESS
         self.value = value
 
     def __bool__(self):
