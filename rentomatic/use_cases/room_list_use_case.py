a28222a42b631ceb0f105a33ff26d3e1f42a7f8b
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-07 08:46:56 +0000

    Chapter 3 - Error management in a use case

--- rentomatic/use_cases/room_list_use_case.py
+++ rentomatic/use_cases/room_list_use_case.py
@@ -1,11 +1,19 @@
 from rentomatic.response_objects import response_objects as res
 
 
-class RoomListUseCase:
+class RoomListUseCase(object):
 
     def __init__(self, repo):
         self.repo = repo
 
-    def execute(self, request):
-        rooms = self.repo.list()
-        return res.ResponseSuccess(rooms)
+    def execute(self, request_object):
+        if not request_object:
+            return res.ResponseFailure.build_from_invalid_request_object(
+                request_object)
+
+        try:
+            rooms = self.repo.list(filters=request_object.filters)
+            return res.ResponseSuccess(rooms)
+        except Exception as exc:
+            return res.ResponseFailure.build_system_error(
+                "{}: {}".format(exc.__class__.__name__, "{}".format(exc)))
--- tests/use_cases/test_room_list_use_case.py
+++ tests/use_cases/test_room_list_use_case.py
@@ -5,6 +5,7 @@ from unittest import mock
 from rentomatic.domain import room as r
 from rentomatic.use_cases import room_list_use_case as uc
 from rentomatic.request_objects import room_list_request_object as req
+from rentomatic.response_objects import response_objects as res
 
 
 @pytest.fixture
@@ -54,5 +55,52 @@ def test_room_list_without_parameters(domain_rooms):
     response = room_list_use_case.execute(request)
 
     assert bool(response) is True
-    repo.list.assert_called_with()
+    repo.list.assert_called_with(filters=None)
     assert response.value == domain_rooms
+
+
+def test_room_list_with_filters(domain_rooms):
+    repo = mock.Mock()
+    repo.list.return_value = domain_rooms
+
+    room_list_use_case = uc.RoomListUseCase(repo)
+    qry_filters = {'code__eq': 5}
+    request_object = req.RoomListRequestObject.from_dict(
+        {'filters': qry_filters})
+
+    response_object = room_list_use_case.execute(request_object)
+
+    assert bool(response_object) is True
+    repo.list.assert_called_with(filters=qry_filters)
+    assert response_object.value == domain_rooms
+
+
+def test_room_list_handles_generic_error():
+    repo = mock.Mock()
+    repo.list.side_effect = Exception('Just an error message')
+
+    room_list_use_case = uc.RoomListUseCase(repo)
+    request_object = req.RoomListRequestObject.from_dict({})
+
+    response_object = room_list_use_case.execute(request_object)
+
+    assert bool(response_object) is False
+    assert response_object.value == {
+        'type': res.ResponseFailure.SYSTEM_ERROR,
+        'message': "Exception: Just an error message"
+    }
+
+
+def test_room_list_handles_bad_request():
+    repo = mock.Mock()
+
+    room_list_use_case = uc.RoomListUseCase(repo)
+    request_object = req.RoomListRequestObject.from_dict({'filters': 5})
+
+    response_object = room_list_use_case.execute(request_object)
+
+    assert bool(response_object) is False
+    assert response_object.value == {
+        'type': res.ResponseFailure.PARAMETERS_ERROR,
+        'message': "filters: Is not iterable"
+    }
