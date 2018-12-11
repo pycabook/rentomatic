26c4ad0ad877e6e967a8c7cf0c40f8983674154a
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-11 09:11:37 +0000

    Chapter 4 - Database fixtures

--- tests/repository/postgres/conftest.py
+++ tests/repository/postgres/conftest.py
@@ -4,6 +4,8 @@ import sqlalchemy_utils
 
 import pytest
 
+from rentomatic.repository.postgres_objects import Base, Room
+
 
 def pg_is_responsive(ip, docker_setup):
     try:
@@ -42,3 +44,70 @@ def pg_engine(docker_ip, docker_services, docker_setup):
     yield engine
 
     conn.close()
+
+
+@pytest.fixture(scope='session')
+def pg_session_empty(pg_engine):
+    Base.metadata.create_all(pg_engine)
+
+    Base.metadata.bind = pg_engine
+
+    DBSession = sqlalchemy.orm.sessionmaker(bind=pg_engine)
+
+    session = DBSession()
+
+    yield session
+
+    session.close()
+
+
+@pytest.fixture(scope='function')
+def pg_data():
+    return [
+        {
+            'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35a',
+            'size': 215,
+            'price': 39,
+            'longitude': -0.09998975,
+            'latitude': 51.75436293,
+        },
+        {
+            'code': 'fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a',
+            'size': 405,
+            'price': 66,
+            'longitude': 0.18228006,
+            'latitude': 51.74640997,
+        },
+        {
+            'code': '913694c6-435a-4366-ba0d-da5334a611b2',
+            'size': 56,
+            'price': 60,
+            'longitude': 0.27891577,
+            'latitude': 51.45994069,
+        },
+        {
+            'code': 'eed76e77-55c1-41ce-985d-ca49bf6c0585',
+            'size': 93,
+            'price': 48,
+            'longitude': 0.33894476,
+            'latitude': 51.39916678,
+        }
+    ]
+
+
+@pytest.fixture(scope='function')
+def pg_session(pg_session_empty, pg_data):
+    for r in pg_data:
+        new_room = Room(
+            code=r['code'],
+            size=r['size'],
+            price=r['price'],
+            longitude=r['longitude'],
+            latitude=r['latitude']
+        )
+        pg_session_empty.add(new_room)
+        pg_session_empty.commit()
+
+    yield pg_session_empty
+
+    pg_session_empty.query(Room).delete()
