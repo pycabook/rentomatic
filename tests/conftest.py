d8eb72f87981020f7223dde1d128f5dde049fa5a
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-11 09:04:17 +0000

    Chapter 4 - Label integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,14 @@ from rentomatic.flask_settings import TestConfig
 @pytest.yield_fixture(scope='function')
 def app():
     return create_app(TestConfig)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--integration", action="store_true",
+                     help="run integration tests")
+
+
+def pytest_runtest_setup(item):
+    if 'integration' in item.keywords and not \
+            item.config.getvalue("integration"):
+        pytest.skip("need --integration option to run")
