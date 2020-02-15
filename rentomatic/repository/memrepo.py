cb1553ee7fa995fb82f23caa171cce9cc56f532f
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-02-15 12:28:21 +0000

    Chapter 3 - The repository

--- rentomatic/repository/memrepo.py
+++ rentomatic/repository/memrepo.py
@@ -5,5 +5,23 @@ class MemRepo:
     def __init__(self, data):
         self.data = data
 
-    def list(self):
-        return [r.Room.from_dict(i) for i in self.data]
+    def list(self, filters=None):
+
+        result = [r.Room.from_dict(i) for i in self.data]
+
+        if filters is None:
+            return result
+
+        if 'code__eq' in filters:
+            result = [r for r in result if r.code == filters['code__eq']]
+
+        if 'price__eq' in filters:
+            result = [r for r in result if r.price == int(filters['price__eq'])]
+
+        if 'price__lt' in filters:
+            result = [r for r in result if r.price < int(filters['price__lt'])]
+
+        if 'price__gt' in filters:
+            result = [r for r in result if r.price > int(filters['price__gt'])]
+
+        return result
