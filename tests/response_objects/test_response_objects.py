96f6c6181657b387597509a47adabc2b51018e6f
staticdev <thiagocavila@gmail.com>
2020-08-29 17:59:43 -0300

    Missing fixture

--- tests/response_objects/test_response_objects.py
+++ tests/response_objects/test_response_objects.py
@@ -49,7 +49,7 @@ def test_response_failure_contains_value(response_type, response_message):
         'type': response_type, 'message': response_message}
 
 
-def test_response_failure_initialisation_with_exception():
+def test_response_failure_initialisation_with_exception(response_type):
     response = res.ResponseFailure(
         response_type, Exception('Just an error message'))
 
