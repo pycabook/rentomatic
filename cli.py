e2fb95de07a1c15df0ac1f13432fce39d4de715a
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-10-24 16:05:26 +0100

    Chapter 3 - Step 9 - CLI Initial data

--- cli.py
+++ cli.py
@@ -3,7 +3,38 @@
 from rentomatic.repository.memrepo import MemRepo
 from rentomatic.use_cases.room_list import room_list_use_case
 
-repo = MemRepo([])
+rooms = [
+    {
+        "code": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+        "size": 215,
+        "price": 39,
+        "longitude": -0.09998975,
+        "latitude": 51.75436293,
+    },
+    {
+        "code": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+        "size": 405,
+        "price": 66,
+        "longitude": 0.18228006,
+        "latitude": 51.74640997,
+    },
+    {
+        "code": "913694c6-435a-4366-ba0d-da5334a611b2",
+        "size": 56,
+        "price": 60,
+        "longitude": 0.27891577,
+        "latitude": 51.45994069,
+    },
+    {
+        "code": "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+        "size": 93,
+        "price": 48,
+        "longitude": 0.33894476,
+        "latitude": 51.39916678,
+    },
+]
+
+repo = MemRepo(rooms)
 result = room_list_use_case(repo)
 
-print(result)
+print([room.to_dict() for room in result])
