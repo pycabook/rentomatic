18aaa64f1f55de23bd196fb782e24ad770ab5cfc
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-04 11:57:20 +0000

    Chapter 5 - Step 7 - Fix external system HTTP server

--- tests/rest/test_room.py
+++ tests/rest/test_room.py
@@ -2,6 +2,7 @@ import json
 from unittest import mock
 
 from rentomatic.domain.room import Room
+from rentomatic.responses import ResponseSuccess
 
 room_dict = {
     "code": "3251a5bd-86be-428d-8ae9-6e51a8048c33",
@@ -16,11 +17,33 @@ rooms = [Room.from_dict(room_dict)]
 
 @mock.patch("application.rest.room.room_list_use_case")
 def test_get(mock_use_case, client):
-    mock_use_case.return_value = rooms
+    mock_use_case.return_value = ResponseSuccess(rooms)
 
     http_response = client.get("/rooms")
 
     assert json.loads(http_response.data.decode("UTF-8")) == [room_dict]
+
     mock_use_case.assert_called()
+    args, kwargs = mock_use_case.call_args
+    assert args[1].filters == {}
+
+    assert http_response.status_code == 200
+    assert http_response.mimetype == "application/json"
+
+
+@mock.patch("application.rest.room.room_list_use_case")
+def test_get_with_filters(mock_use_case, client):
+    mock_use_case.return_value = ResponseSuccess(rooms)
+
+    http_response = client.get(
+        "/rooms?filter_price__gt=2&filter_price__lt=6"
+    )
+
+    assert json.loads(http_response.data.decode("UTF-8")) == [room_dict]
+
+    mock_use_case.assert_called()
+    args, kwargs = mock_use_case.call_args
+    assert args[1].filters == {"price__gt": "2", "price__lt": "6"}
+
     assert http_response.status_code == 200
     assert http_response.mimetype == "application/json"
