feb6b8108a2438331a2c4d3fa04efdfe78906670
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-04 16:36:17 +0000

    Project cleanup

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Rentomatic in a project::
-
-    import rentomatic
--- rentomatic/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Leonardo Giordani"""
-__email__ = 'giordani.leonardo@gmail.com'
-__version__ = '0.1.0'
