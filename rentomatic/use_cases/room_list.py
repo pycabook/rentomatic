24375a634d98f340ec2480d322ef97cb408f14a3
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-04 11:32:55 +0000

    Chapter 3 - Step 6 - Use case

---
+++ rentomatic/use_cases/room_list.py
@@ -0,0 +1,2 @@
+def room_list_use_case(repo):
+    return repo.list()
---
+++ tests/use_cases/test_room_list.py
@@ -0,0 +1,53 @@
+import pytest
+import uuid
+from unittest import mock
+
+from rentomatic.domain.room import Room
+from rentomatic.use_cases.room_list import room_list_use_case
+
+
+@pytest.fixture
+def domain_rooms():
+    room_1 = Room(
+        code=uuid.uuid4(),
+        size=215,
+        price=39,
+        longitude=-0.09998975,
+        latitude=51.75436293,
+    )
+
+    room_2 = Room(
+        code=uuid.uuid4(),
+        size=405,
+        price=66,
+        longitude=0.18228006,
+        latitude=51.74640997,
+    )
+
+    room_3 = Room(
+        code=uuid.uuid4(),
+        size=56,
+        price=60,
+        longitude=0.27891577,
+        latitude=51.45994069,
+    )
+
+    room_4 = Room(
+        code=uuid.uuid4(),
+        size=93,
+        price=48,
+        longitude=0.33894476,
+        latitude=51.39916678,
+    )
+
+    return [room_1, room_2, room_3, room_4]
+
+
+def test_room_list_without_parameters(domain_rooms):
+    repo = mock.Mock()
+    repo.list.return_value = domain_rooms
+
+    result = room_list_use_case(repo)
+
+    repo.list.assert_called_with()
+    assert result == domain_rooms
