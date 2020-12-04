9750fce9ffd37eafe9494d35fa6f729644c0387a
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-04 12:03:20 +0000

    Chapter 6 - Step 1 - Label integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,16 @@ def app():
     app = create_app("testing")
 
     return app
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--integration", action="store_true", help="run integration tests"
+    )
+
+
+def pytest_runtest_setup(item):
+    if "integration" in item.keywords and not item.config.getvalue(
+        "integration"
+    ):
+        pytest.skip("need --integration option to run")
