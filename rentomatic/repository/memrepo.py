34b0315cbb29928edb5ca1564888f3843dde918b
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-04 11:53:11 +0000

    Chapter 5 - Step 8 - Fix external system repository

--- rentomatic/repository/memrepo.py
+++ rentomatic/repository/memrepo.py
@@ -5,5 +5,29 @@ class MemRepo:
     def __init__(self, data):
         self.data = data
 
-    def list(self):
-        return [Room.from_dict(i) for i in self.data]
+    def list(self, filters=None):
+
+        result = [Room.from_dict(i) for i in self.data]
+
+        if filters is None:
+            return result
+
+        if "code__eq" in filters:
+            result = [r for r in result if r.code == filters["code__eq"]]
+
+        if "price__eq" in filters:
+            result = [
+                r for r in result if r.price == int(filters["price__eq"])
+            ]
+
+        if "price__lt" in filters:
+            result = [
+                r for r in result if r.price < int(filters["price__lt"])
+            ]
+
+        if "price__gt" in filters:
+            result = [
+                r for r in result if r.price > int(filters["price__gt"])
+            ]
+
+        return result
--- tests/repository/test_memrepo.py
+++ tests/repository/test_memrepo.py
@@ -44,3 +44,64 @@ def test_repository_list_without_parameters(room_dicts):
     rooms = [Room.from_dict(i) for i in room_dicts]
 
     assert repo.list() == rooms
+
+
+def test_repository_list_with_code_equal_filter(room_dicts):
+    repo = MemRepo(room_dicts)
+
+    rooms = repo.list(
+        filters={"code__eq": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a"}
+    )
+
+    assert len(rooms) == 1
+    assert rooms[0].code == "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a"
+
+
+def test_repository_list_with_price_equal_filter(room_dicts):
+    repo = MemRepo(room_dicts)
+
+    rooms = repo.list(filters={"price__eq": 60})
+
+    assert len(rooms) == 1
+    assert rooms[0].code == "913694c6-435a-4366-ba0d-da5334a611b2"
+
+
+def test_repository_list_with_price_less_than_filter(room_dicts):
+    repo = MemRepo(room_dicts)
+
+    rooms = repo.list(filters={"price__lt": 60})
+
+    assert len(rooms) == 2
+    assert set([r.code for r in rooms]) == {
+        "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+        "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+    }
+
+
+def test_repository_list_with_price_greater_than_filter(room_dicts):
+    repo = MemRepo(room_dicts)
+
+    rooms = repo.list(filters={"price__gt": 48})
+
+    assert len(rooms) == 2
+    assert set([r.code for r in rooms]) == {
+        "913694c6-435a-4366-ba0d-da5334a611b2",
+        "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+    }
+
+
+def test_repository_list_with_price_between_filter(room_dicts):
+    repo = MemRepo(room_dicts)
+
+    rooms = repo.list(filters={"price__lt": 66, "price__gt": 48})
+
+    assert len(rooms) == 1
+    assert rooms[0].code == "913694c6-435a-4366-ba0d-da5334a611b2"
+
+
+def test_repository_list_price_as_strings(room_dicts):
+    repo = MemRepo(room_dicts)
+
+    repo.list(filters={"price__eq": "60"})
+    repo.list(filters={"price__lt": "60"})
+    repo.list(filters={"price__gt": "60"})
