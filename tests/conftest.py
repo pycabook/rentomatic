8eca5dddd77737b5436e50687bdf068f68276f91
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-05 19:51:19 +0000

    Chapter 6 - Step 4 - Postgres fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
-
 from application.app import create_app
+from manage import read_json_configuration
 
 
 @pytest.fixture
@@ -9,6 +9,11 @@ def app():
     app = create_app("testing")
 
     return app
+
+
+@pytest.fixture(scope="session")
+def app_configuration():
+    return read_json_configuration("testing")
 
 
 def pytest_addoption(parser):
