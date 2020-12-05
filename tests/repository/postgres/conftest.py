46fa6311de50579943ce3e85d332dad970bd8dbe
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-05 19:51:19 +0000

    Chapter 6 - Step 4 - Postgres fixtures

---
+++ tests/repository/postgres/conftest.py
@@ -0,0 +1,80 @@
+import sqlalchemy
+import pytest
+
+from rentomatic.repository.postgres_objects import Base, Room
+
+
+@pytest.fixture(scope="session")
+def pg_session_empty(app_configuration):
+    conn_str = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+        app_configuration["POSTGRES_USER"],
+        app_configuration["POSTGRES_PASSWORD"],
+        app_configuration["POSTGRES_HOSTNAME"],
+        app_configuration["POSTGRES_PORT"],
+        app_configuration["APPLICATION_DB"],
+    )
+    engine = sqlalchemy.create_engine(conn_str)
+    connection = engine.connect()
+
+    Base.metadata.create_all(engine)
+    Base.metadata.bind = engine
+
+    DBSession = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = DBSession()
+
+    yield session
+
+    session.close()
+    connection.close
+
+
+@pytest.fixture(scope="session")
+def pg_test_data():
+    return [
+        {
+            "code": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+            "size": 215,
+            "price": 39,
+            "longitude": -0.09998975,
+            "latitude": 51.75436293,
+        },
+        {
+            "code": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+            "size": 405,
+            "price": 66,
+            "longitude": 0.18228006,
+            "latitude": 51.74640997,
+        },
+        {
+            "code": "913694c6-435a-4366-ba0d-da5334a611b2",
+            "size": 56,
+            "price": 60,
+            "longitude": 0.27891577,
+            "latitude": 51.45994069,
+        },
+        {
+            "code": "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+            "size": 93,
+            "price": 48,
+            "longitude": 0.33894476,
+            "latitude": 51.39916678,
+        },
+    ]
+
+
+@pytest.fixture(scope="function")
+def pg_session(pg_session_empty, pg_test_data):
+    for r in pg_test_data:
+        new_room = Room(
+            code=r["code"],
+            size=r["size"],
+            price=r["price"],
+            longitude=r["longitude"],
+            latitude=r["latitude"],
+        )
+        pg_session_empty.add(new_room)
+        pg_session_empty.commit()
+
+    yield pg_session_empty
+
+    pg_session_empty.query(Room).delete()
