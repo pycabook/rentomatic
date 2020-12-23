75d5fe7dc573ba9b2635c889dcd6e425cccd6d11
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-23 20:06:51 +0000

    Chapter 6 - Step 5 - Integration tests

---
+++ rentomatic/repository/postgresrepo.py
@@ -0,0 +1,55 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from rentomatic.domain import room
+from rentomatic.repository.postgres_objects import Base, Room
+
+
+class PostgresRepo:
+    def __init__(self, configuration):
+        connection_string = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+            configuration["POSTGRES_USER"],
+            configuration["POSTGRES_PASSWORD"],
+            configuration["POSTGRES_HOSTNAME"],
+            configuration["POSTGRES_PORT"],
+            configuration["APPLICATION_DB"],
+        )
+
+        self.engine = create_engine(connection_string)
+        Base.metadata.create_all(self.engine)
+        Base.metadata.bind = self.engine
+
+    def _create_room_objects(self, results):
+        return [
+            room.Room(
+                code=q.code,
+                size=q.size,
+                price=q.price,
+                latitude=q.latitude,
+                longitude=q.longitude,
+            )
+            for q in results
+        ]
+
+    def list(self, filters=None):
+        DBSession = sessionmaker(bind=self.engine)
+        session = DBSession()
+
+        query = session.query(Room)
+
+        if filters is None:
+            return self._create_room_objects(query.all())
+
+        if "code__eq" in filters:
+            query = query.filter(Room.code == filters["code__eq"])
+
+        if "price__eq" in filters:
+            query = query.filter(Room.price == filters["price__eq"])
+
+        if "price__lt" in filters:
+            query = query.filter(Room.price < filters["price__lt"])
+
+        if "price__gt" in filters:
+            query = query.filter(Room.price > filters["price__gt"])
+
+        return self._create_room_objects(query.all())
