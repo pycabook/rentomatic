5f4a3a4e3b23d6ff278f625b45859d3155127787
Leonardo Giordani <giordani.leonardo@gmail.com>
2021-08-17 10:27:35 +0100

    Chapter 5 - Step 7 - Fix external system HTTP server

--- application/rest/room.py
+++ application/rest/room.py
@@ -1,12 +1,21 @@
 import json
 
-from flask import Blueprint, Response
+from flask import Blueprint, request, Response
 
 from rentomatic.repository.memrepo import MemRepo
 from rentomatic.use_cases.room_list import room_list_use_case
 from rentomatic.serializers.room import RoomJsonEncoder
+from rentomatic.requests.room_list import build_room_list_request
+from rentomatic.responses import ResponseTypes
 
 blueprint = Blueprint("room", __name__)
+
+STATUS_CODES = {
+    ResponseTypes.SUCCESS: 200,
+    ResponseTypes.RESOURCE_ERROR: 404,
+    ResponseTypes.PARAMETERS_ERROR: 400,
+    ResponseTypes.SYSTEM_ERROR: 500,
+}
 
 rooms = [
     {
@@ -42,11 +51,23 @@ rooms = [
 
 @blueprint.route("/rooms", methods=["GET"])
 def room_list():
+    qrystr_params = {
+        "filters": {},
+    }
+
+    for arg, values in request.args.items():
+        if arg.startswith("filter_"):
+            qrystr_params["filters"][arg.replace("filter_", "")] = values
+
+    request_object = build_room_list_request(
+        filters=qrystr_params["filters"]
+    )
+
     repo = MemRepo(rooms)
-    result = room_list_use_case(repo)
+    response = room_list_use_case(repo, request_object)
 
     return Response(
-        json.dumps(result, cls=RoomJsonEncoder),
+        json.dumps(response.value, cls=RoomJsonEncoder),
         mimetype="application/json",
-        status=200,
+        status=STATUS_CODES[response.type],
     )
--- tests/rest/test_room.py
+++ tests/rest/test_room.py
@@ -1,7 +1,14 @@
 import json
 from unittest import mock
 
+import pytest
+
 from rentomatic.domain.room import Room
+from rentomatic.responses import (
+    ResponseFailure,
+    ResponseSuccess,
+    ResponseTypes,
+)
 
 room_dict = {
     "code": "3251a5bd-86be-428d-8ae9-6e51a8048c33",
@@ -16,11 +23,60 @@ rooms = [Room.from_dict(room_dict)]
 
 @mock.patch("application.rest.room.room_list_use_case")
 def test_get(mock_use_case, client):
-    mock_use_case.return_value = rooms
+    mock_use_case.return_value = ResponseSuccess(rooms)
 
     http_response = client.get("/rooms")
 
     assert json.loads(http_response.data.decode("UTF-8")) == [room_dict]
+
     mock_use_case.assert_called()
+    args, kwargs = mock_use_case.call_args
+    assert args[1].filters == {}
+
     assert http_response.status_code == 200
     assert http_response.mimetype == "application/json"
+
+
+@mock.patch("application.rest.room.room_list_use_case")
+def test_get_with_filters(mock_use_case, client):
+    mock_use_case.return_value = ResponseSuccess(rooms)
+
+    http_response = client.get(
+        "/rooms?filter_price__gt=2&filter_price__lt=6"
+    )
+
+    assert json.loads(http_response.data.decode("UTF-8")) == [room_dict]
+
+    mock_use_case.assert_called()
+    args, kwargs = mock_use_case.call_args
+    assert args[1].filters == {"price__gt": "2", "price__lt": "6"}
+
+    assert http_response.status_code == 200
+    assert http_response.mimetype == "application/json"
+
+
+@pytest.mark.parametrize(
+    "response_type, expected_status_code",
+    [
+        (ResponseTypes.PARAMETERS_ERROR, 400),
+        (ResponseTypes.RESOURCE_ERROR, 404),
+        (ResponseTypes.SYSTEM_ERROR, 500),
+    ],
+)
+@mock.patch("application.rest.room.room_list_use_case")
+def test_get_response_failures(
+    mock_use_case,
+    client,
+    response_type,
+    expected_status_code,
+):
+    mock_use_case.return_value = ResponseFailure(
+        response_type,
+        message="Just an error message",
+    )
+
+    http_response = client.get("/rooms?dummy_request_string")
+
+    mock_use_case.assert_called()
+
+    assert http_response.status_code == expected_status_code
