87bac100700be20f5373325297420bf10b1ec837
Leonardo Giordani <giordani.leonardo@gmail.com>
2023-05-07 16:05:11 +0100

    Chapter 6 - Step 4 - Postgres fixtures

--- tests/repository/postgres/test_postgresrepo.py
+++ tests/repository/postgres/test_postgresrepo.py
@@ -1,7 +1,8 @@
 import pytest
+from rentomatic.repository.postgres_objects import Room
 
 pytestmark = pytest.mark.integration
 
 
-def test_dummy():
-    pass
+def test_dummy(pg_session):
+    assert len(pg_session.query(Room).all()) == 4
