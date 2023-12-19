cde398553ebd889d05ea9085a4681d06b5af47c7
sub <hslim2@naver.com>
2023-12-19 16:46:40 +0900

    first commit

---
+++ domain/room.py
@@ -0,0 +1,17 @@
+import uuid
+import dataclasses
+
+@dataclasses.dataclass
+class Room:
+    code: uuid.UUID
+    size: int
+    price: int
+    longitude: float
+    latitude: float
+
+    @classmethod
+    def from_dict(self, d):
+        return self(**d)
+
+    def to_dict(self):
+        return dataclasses.asdict(self)
---
+++ tests/domain/test_room.py
@@ -0,0 +1,49 @@
+import uuid
+from rentomatic.domain.room import Room
+
+def test_room_model_init():
+    code = uuid.uuid4()
+    room = Room(
+        code,
+        size=200,
+        price=10,
+        longitude = -0.09998975,
+        latitude = 51.75436293,
+    )
+
+    assert room.code == code
+    assert room.size == 200
+    assert room.price == 10
+    assert room.longitude == -0.09998975
+    assert room.latitude == 51.75436293
+
+def test_room_model_from_dict():
+    code = uuid.uuid4()
+    init_dict = {
+        "code" : code,
+        "size" : 200,
+        "price" : 10,
+        "longitude": -0.09998975,
+        "latitude" : 51.75436293,
+    }
+
+    room = Room.from_dict(init_dict)
+
+    assert room.code == code
+    assert room.size == 200
+    assert room.price == 10
+    assert room.longitude == -0.09998975
+    assert room.latitude == 51.75436293
+
+
+def test_room_model_to_dict():
+    init_dict = {
+        "code" : uuid.uuid4(),
+        "size" : 200,
+        "price" : 10,
+        "longtitude" : -0.09998975,
+        "latitude" : 51.75436293,
+    }
+    room = Room.from_dict(init_dict)
+
+    assert room.to_dict() == init_dict
